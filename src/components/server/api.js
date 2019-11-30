5d07e56b2cb34c48474dbb58d6d38e2a55b046df
RealRather <36735076+RealRather@users.noreply.github.com>
2019-11-30 19:19:54 +0300

    Delete api.js

--- src/components/server/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = () => axios.create({
-        baseURL: `https://api.exchangeratesapi.io`,
-        withCredentials: false,
-        timeout: 5000
-    });
-
-export default api;
